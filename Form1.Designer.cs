1107dc13d082c83a73c0133ae8bf1ab568287906
Nicolas Herrera <zambottinico@gmail.com>
2022-10-22 18:37:18 -0300

    primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace reportes2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.txtParam = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.conjuntoDatos = new reportes2.ConjuntoDatos();
+            this.verArticulosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.verArticulosTableAdapter = new reportes2.ConjuntoDatosTableAdapters.VerArticulosTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.conjuntoDatos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verArticulosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtParam
+            // 
+            this.txtParam.Location = new System.Drawing.Point(444, 41);
+            this.txtParam.Name = "txtParam";
+            this.txtParam.Size = new System.Drawing.Size(176, 20);
+            this.txtParam.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(626, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "tabla";
+            reportDataSource1.Value = this.verArticulosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "reportes2.informe.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(19, 68);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(682, 370);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // conjuntoDatos
+            // 
+            this.conjuntoDatos.DataSetName = "ConjuntoDatos";
+            this.conjuntoDatos.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // verArticulosBindingSource
+            // 
+            this.verArticulosBindingSource.DataMember = "VerArticulos";
+            this.verArticulosBindingSource.DataSource = this.conjuntoDatos;
+            // 
+            // verArticulosTableAdapter
+            // 
+            this.verArticulosTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CONSULTAR ARTICULOS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtParam);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.conjuntoDatos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verArticulosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtParam;
+        private System.Windows.Forms.Button btnBuscar;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource verArticulosBindingSource;
+        private ConjuntoDatos conjuntoDatos;
+        private ConjuntoDatosTableAdapters.VerArticulosTableAdapter verArticulosTableAdapter;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reportes2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string parametro { get; set; }
+            
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            parametro = "";
+            // TODO: esta línea de código carga datos en la tabla 'conjuntoDatos.VerArticulos' Puede moverla o quitarla según sea necesario.
+            this.verArticulosTableAdapter.Fill(this.conjuntoDatos.VerArticulos, parametro);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            parametro = txtParam.Text;
+
+            this.verArticulosTableAdapter.Fill(this.conjuntoDatos.VerArticulos, parametro);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
